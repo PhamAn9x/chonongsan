6b5d188f96600ce0e5c7a8c204c0f5e7c4d21d80
An <u.caca96@gmail.com>
2018-08-07 20:07:02 +0700

    moi1

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="css/boxsp.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/demo.css" media="all" />
+	<script type="text/javascript" src="js/boxsp.js"></script>
 	<title></title>
 </head>
-<body>
+<body style="width: 99%; margin-left: 0.5%;">
 	<div class="w3-row">
 		<div class="w3-col s12 w3-black w3-xxxlarge">
 			<img src="logo_image/banner.png">
 		</div>
 	</div>
-	<div class="w3-row">
+	<div class="w3-row" style="margin-top: 1%;">
 		<div class="w3-col s2 "><img src="logo_image/logo.png"></div>
-		<div class="w3-col s6 ">
+		<div class="w3-col s10 ">
 			<form name="frtimkiem" id="frtimkiem" method="get">
 				<table>
 					<tr>
@@ -22,30 +26,23 @@
 									<option>Chọn danh mục</option>
 								</select>
 							</div>
-							<div  style="position: absolute; left: 36%; width: 300px;">
+							<div style="float: left; padding-left: 10px;">
 								<input class="w3-input w3-border" type="tetx" name="iptimkiem" id="iptimkiem" placeholder="Nhập từ khóa cần tìm">
 							</div>	
-							<div>
-								<button class="w3-btn w3-round w3-green" name="btntimkiem" id="btntimkiem" style="position: absolute; left: 59%;"> Tìm kiếm </button>
+							<div style="float: left; margin-left:5px; width: 150px;">
+								<button class="w3-btn w3-round w3-green" name="btntimkiem" id="btntimkiem" style=" width: 125px;margin-left: 5px;" ><span style="font-weight: 700;">Tìm kiếm</span></button>
 							</div>						
 						</td>
-					</tr>
-				</table>
-			</form>
-		</div>
-		<div class="w3-col s4">
-					<table>
-						<tr>
 							<td
-								style=" font-size: 14px; font-weight: 800; float: right; margin-top:4px;"><img src="logo_image/ictim.png" width="30px" height="30px">Thích(0)
+								style=" font-size: 16px; font-weight: 800; float:left; margin-top:10px; margin-right: 7px;"><img src="logo_image/ictim.png" width="30px" height="30px"><a href="" style=" text-decoration: none;"> Thích(0)</a>
 							</td>
-							<td class="w3-border-left w3-border-right" style="font-weight: 600; padding-top: 6px; padding-left: 3px; font-size: 14px;">
-								<img style="margin-left:3px;" src="logo_image/icondn.png" width="25px" height="25px"><span style="padding-left: 7px;">Đăng nhập</span>  
+							<td class="w3-border-left w3-border-right" style="font-weight: 600; padding-top: 3px; padding-left: 3px; font-size: 16px;">
+								<img style="margin-left:7px;" src="logo_image/icondn.png" width="25px" height="25px"><span style="padding-left: 7px;"><a href="" style=" text-decoration: none;"> Đăng nhập </a></span>  
 								<span style="font-size: 17px;">/</span>
-								<span style="padding-right:3px;">Đăng ký</span>  
+								<span style="padding-right:7px;"><a href="" style=" text-decoration: none;">Đăng ký</a></span>  
 							</td>
 							<td>
-								<button class="w3-btn w3-round w3-red" style="margin-top: 4px; margin-left: 5px;">Đăng tin</button>
+								<a href="#"><button  class="w3-btn w3-round w3-red" style="margin-top: 4px; margin-left: 7px; width: 125px;";><span style="font-weight: 700">Đăng tin</span></button></a>
 							</td>
 
 						</tr>
@@ -53,8 +50,13 @@
 			</div>
 		</div>
 	</div>
-	<div class="w3-row">
-		<div class="w3-col s12 w3-blue w3-xxxlarge">san pham hot</div>
+	<div class="w3-row" >
+		<div class="w3-col s12" style="margin-bottom: 3%;">
+			<?php 
+				include("views/sanphammoi.php");
+			?>
+
+		</div
 	</div>
 	<div class="w3-row">
 		<div class="w3-col s2 w3-blue w3-xxxlarge">Danh muc sp</div>
---
+++ views/sanphammoi.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thiết kế menu nhà hàng với hiệu ứng hover bằng CSS3</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/demo.css" />
+    <link rel="icon" href="http://www.thuthuatweb.net/wp-content/themes/HostingSite/favicon.ico" type="image/x-ico"/>
+</head>
+<body>
+<div class="container">
+               
+<!-- start our menu here-->
+<div class="our_menu">
+    <ul class="menu">
+            <li class="item">
+               <strong class="new">new</strong> 
+		          <a href="https://www.thuthuatweb.net">
+                    <h3>2 for $20</h3>
+                    <p> 1 Appetizer + 2 Entrees = Just $20</p>
+                    <img src="images/1.jpg" alt="2 for 20" height="164" width="283">
+                 </a>
+            </li>
+            <li class="item">
+               <strong class="new">new</strong>
+			   <a href="https://www.thuthuatweb.net">
+                    <h3>Appetizers</h3>
+                    <p> Start a meal off right with your favorite from our menu of signature appetizers. Can't choose just one? You don't have</p>
+                    <img src="images/2.jpg" alt="" height="164" width="283">
+                </a>
+            </li>
+            <li class="item">
+            	  <strong class="new">new</strong>
+		       <a href="https://www.thuthuatweb.net">
+                    <h3>Pick 'N Pair Lunches</h3>
+                    <p> Make your own lunch combo with soups, salads, sandwiches &amp; more.</p>
+                    <img src="images/3.jpg" alt="Alternate" height="164" width="283">
+               </a>
+            </li>
+            <li class="item">
+			   <a href="https://www.thuthuatweb.net">
+                    <h3> Entrees &amp; Main Dishes</h3>
+                    <p>Our menu opens up a world of choices, loaded with flavor. From juicy steaks and succulent seafood, to saucy ribs and  burgers.</p>
+                    <img src="images/4.jpg" alt="" height="164" width="283">
+               </a>
+            </li>
+            <li class="item">
+		      <a href="https://www.thuthuatweb.net">
+                    <h3>Sizzling Entrees</h3>
+                    <p> Fresh flavor never sounded so good.</p>
+                    <img src="images/5.jpg" alt="" height="164" width="283">
+              </a>
+            </li>
+            <li class="item">
+			   <a href="https://www.thuthuatweb.net">
+                    <h3> Freshburgers &amp; Sandwiches</h3>
+                    <p> From mouthwatering Freshburgers to big and tasty Sandwiches, we've got the flavors to satisfy your cravings.</p>
+                    <img src="images/6.jpg" alt="Alternate" height="164" width="283">
+               </a>
+            </li>
+            <li class="item">
+			  <a href="https://www.thuthuatweb.net">
+                    <h3>Salads</h3>
+                    <p>Fresh &amp; Delicious</p>
+                    <img src="images/7.jpg" alt="Alternate" height="164" width="283">
+              </a>
+            </li>
+            <li class="item">
+			   <a href="https://www.thuthuatweb.net">
+                    <h3> Weight Watchers</h3>
+                    <p>Delicious Entrées endorsed by Weight Watchers. Eating Right Never Tasted So Good.</p>
+                    <img src="images/8.jpg" alt="Alternate" height="164" width="283">
+                </a>
+            </li>
+            <li class="item">
+			   <a href="https://www.thuthuatweb.net">
+                    <h3>Under 550 Calories</h3>
+                    <p> Big Flavor. Big Portions. Big Value. All Under 550 Calories.</p>
+                    <img src="images/9.jpg" alt="Alternate" height="164" width="283">
+               </a>
+            </li>
+   </ul>     
+</div>
+<!-- end our menu -->
+    
+</div>
+ 
+<div class="footer-bar">
+    <span class="article-wrapper">
+        <span class="article-label">Xem Bài Viết: </span>
+        <span class="article-link"><a href="https://www.thuthuatweb.net/css/thiet-ke-menu-nha-hang-voi-hieu-ung-hover-bang-css3.html">Thiết kế menu nhà hàng với hiệu ứng hover bằng CSS3</a></span>
+    </span>
+    
+</div>
+    
+</body>
+</html>
