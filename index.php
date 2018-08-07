9539d366bfde71caf46c43f1207214cbd40658c5
An <u.caca96@gmail.com>
2018-08-07 15:02:02 +0700

    moi3

--- index.php
+++ index.php
@@ -1,9 +1,56 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title></title>
 </head>
 <body>
-xin chao
+	<div class="w3-row">
+		<div class="w3-col s12 w3-black w3-xxxlarge">
+			<img src="logo_image/banner.png">
+		</div>
+	</div>
+	<div class="w3-row">
+		<div class="w3-col s2 "><img src="logo_image/logo.png"></div>
+		<div class="w3-col s6 ">
+			<form name="frtimkiem" id="frtimkiem" method="get">
+				<table>
+					<tr>
+						<td>
+							<div style="float: left;">
+								<select class="w3-input w3-border" style="width: 250px;">
+									<option>Chọn danh mục</option>
+								</select>
+							</div>
+							<div  style="position: absolute; left: 36%; width: 300px;">
+								<input class="w3-input w3-border" type="tetx" name="iptimkiem" id="iptimkiem" placeholder="Nhập từ khóa cần tìm">
+							</div>	
+							<div>
+								<button class="w3-btn w3-round w3-green" name="btntimkiem" id="btntimkiem" style="position: absolute; left: 59%;"> Tìm kiếm </button>
+							</div>						
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+		<div class="w3-col s4">
+					<table>
+						<tr>
+							<td
+								style="font-weight: 800; float: right;"><img src="logo_image/ictim.png" width="30px" height="30px">Thích(0)
+							</td>
+						</tr>
+					</table>
+			</div>
+		</div>
+	</div>
+	<div class="w3-row">
+		<div class="w3-col s12 w3-blue w3-xxxlarge">san pham hot</div>
+	</div>
+	<div class="w3-row">
+		<div class="w3-col s2 w3-blue w3-xxxlarge">Danh muc sp</div>
+		<div class="w3-col s8 w3-red w3-xxxlarge">gian hang</div>
+		<div class="w3-col s2 w3-yellow w3-xxxlarge">tim kiem nc</div>
+	</div>
 </body>
 </html>
