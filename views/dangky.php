2ba425333ce5bd8e58583fbd2399f4749fbd1ee3
An <u.caca96@gmail.com>
2018-08-07 11:49:01 +0700

    moi

---
+++ views/dangky.php
@@ -0,0 +1,32 @@
+<!-- header starts here -->
+<link rel="stylesheet" type="text/css" href="./css/dangky.css">
+<!-- header ends here -->
+<div class="loginbox radius">
+  <h2 style="color:#141823; text-align:center;">ĐĂNG KÝ THÀNH VIÊN</h2>
+  <div class="loginboxinner radius">
+    <div class="loginheader">
+      <h4 class="title">Nhập đầy đủ thông tin để tiến hành dăng ký!.</h4>
+    </div>
+    <!--loginheader-->
+    <div class="loginform">
+      <form id="login" action="" method="post">
+        <p>
+          <input type="text" id="sdt" name="sdt" placeholder="Nhập số điện thoại" class="radius" />
+        </p>
+        <p>
+          <input type="password" id="matkhau" name="matkhau" placeholder="Nhập mật khẩu" class="radius" />
+        </p>
+        <p>
+          <input type="text" id="rematkhau" name="rematkhau" placeholder="Nhập lại mật khẩu" class="radius" />
+        </p>
+        <p>
+        <p>
+          <button class="radius title" name="dangky">Đăng ký</button>
+        </p>
+      </form>
+    </div>
+    <!--loginform-->
+  </div>
+  <!--loginboxinner-->
+</div>
+<!--loginbox-->
