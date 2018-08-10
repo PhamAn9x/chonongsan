8f06fb98a77a859430816efe50b0fdce47a6e933
An <u.caca96@gmail.com>
2018-08-11 00:00:46 +0700

    moi

---
+++ bando/syncing-chart-with-google-maps/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/team/amcharts/pen/GJyeKR.
+
+ This demo shows how to use events to two-way sync a JavaScript Chart and Google Map.
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	<script type="text/javascript" src="js/boxsp.js"></script>
 	<title></title>
 </head>
+<?php include("vendor/vine/index.html"); ?>
 <body style="width: 99%; margin-left: 0.5%; ">
 	<div class="w3-row">
 		<div class="w3-col s8 w3-black">
